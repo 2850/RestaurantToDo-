64c5e984ebc536f1016a08bff29c33ceddfba34c
willis <soeasy2850@hotmail.com>
2020-08-10 17:49:13 +0800

    test

--- hello/views.py
+++ hello/views.py
@@ -25,4 +25,4 @@ def db(request):
 def index(request):
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    return HttpResponse('<pre>' + r.text + '</pre> ')
