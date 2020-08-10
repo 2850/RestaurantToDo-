3a88e4c0184c198ad5680ed0c2ac1f2d23b9240b
willis <soeasy2850@hotmail.com>
2020-08-10 17:19:09 +0800

    demo

--- hello/views.py
+++ hello/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Greeting
+import requests
 
 # Create your views here.
-def index(request):
-    # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+
+
+# def index(request):
+#     # return HttpResponse('Hello from Python!')
+#     return render(request, "index.html")
 
 
 def db(request):
@@ -17,3 +20,9 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, "db.html", {"greetings": greetings})
+
+
+def index(request):
+    r = requests.get('http://httpbin.org/status/418')
+    print(r.text)
+    return HttpResponse('<pre>' + r.text + '</pre>')
